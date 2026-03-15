fa749c15737b1b5f0cc4f371cd7e27fb0468ad26
Neel gupta <neelgupta1557@gmail.com>
2026-03-16 02:44:12 +0530

    Add check function to validate answers

---
+++ script.js
@@ -0,0 +1,10 @@
+function check(ans){
+
+if(ans=="B"){
+document.getElementById("result").innerHTML="✅ Correct Answer";
+}
+else{
+document.getElementById("result").innerHTML="❌ Wrong Answer";
+}
+
+}
