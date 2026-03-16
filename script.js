f097fe6375ed81b995341895190c9a4396823637
Neel gupta <neelgupta1557@gmail.com>
2026-03-17 04:31:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-function check(ans){
+const sheetID = "1AkrVuM6bv-yPdLZTODvS0D73leu0wsIdnMHUVQRx5cM";
+const url = `https://opensheet.elk.sh/${sheetID}/Sheet1`;
 
-if(ans=="B"){
-document.getElementById("result").innerHTML="✅ Correct Answer";
+let pdfData = [];
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+
+pdfData = data;
+displayPDF(data);
+
+});
+
+function displayPDF(data){
+
+let html="";
+
+data.forEach(pdf => {
+
+html += `
+<div class="card">
+<h3>${pdf.Title}</h3>
+<p>${pdf.Category}</p>
+<a href="${pdf.Link}" target="_blank">Open PDF</a>
+</div>
+`;
+
+});
+
+document.getElementById("pdfList").innerHTML = html;
+
 }
-else{
-document.getElementById("result").innerHTML="❌ Wrong Answer";
-}
+
+function searchPDF(){
+
+let input = document.getElementById("search").value.toLowerCase();
+
+let filtered = pdfData.filter(pdf =>
+pdf.Title.toLowerCase().includes(input) ||
+pdf.Category.toLowerCase().includes(input)
+);
+
+displayPDF(filtered);
 
 }
